8397b484533b1bf5fc92f7ff5f4c2319e788dd17
Sathya M <62427113+msathya98@users.noreply.github.com>
2021-01-29 19:43:26 +0530

    2_deploy_contracts.js updated with MytokenSale .

--- 2_deploy_contracts.js
+++ 2_deploy_contracts.js
@@ -1,5 +1,10 @@
 var MyToken = artifacts.require("MyToken.sol");
+var MyTokenSale = artifacts.require("MyTokenSale");
 
 module.exports = async function(deployer) {
-  await deployer.deploy(MyToken, 10000);
+  let addr = await web3.eth.getAccounts();
+  await deployer.deploy(MyToken, 10000)
+  await deployer.deploy(MyTokenSale, 1, addr[0], MyToken.address);
+  let instance = await MyToken.deployed();
+  await instance.transfer(MyTokenSale.address, 10000);
 }
