b7a836132576a390d7d4a8255fccf52ca687ade1
Sathya M <62427113+msathya98@users.noreply.github.com>
2021-02-01 20:03:47 +0530

    2-deploy-contracts.js of tokenization is updated.

--- 2_deploy_contracts.js
+++ 2_deploy_contracts.js
@@ -1,10 +1,11 @@
 var MyToken = artifacts.require("MyToken.sol");
 var MyTokenSale = artifacts.require("MyTokenSale");
+require("dotenv").config({path: "../.env"});
 
 module.exports = async function(deployer) {
   let addr = await web3.eth.getAccounts();
-  await deployer.deploy(MyToken, 10000)
-  await deployer.deploy(MyTokenSale, 1, addr[0], MyToken.address);
+  await deployer.deploy(MyToken, process.env.INITIAL_TOKENS);
   let instance = await MyToken.deployed();
-  await instance.transfer(MyTokenSale.address, 10000);
+  await deployer.deploy(MyTokenSale, 1, addr[0], MyToken.address);
+ await instance.transfer(MyTokenSale.address, process.env.INITIAL_TOKENS);
 }
