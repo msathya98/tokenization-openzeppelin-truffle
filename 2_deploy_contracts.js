2927fe2921eab54b54f73397a11f1b62ebb49662
Sathya M <62427113+msathya98@users.noreply.github.com>
2021-02-02 19:44:00 +0530

    2-deploy-contracts.js of smart-contract updated.

--- 2_deploy_contracts.js
+++ 2_deploy_contracts.js
@@ -1,11 +1,15 @@
-var MyToken = artifacts.require("MyToken.sol");
-var MyTokenSale = artifacts.require("MyTokenSale");
-require("dotenv").config({path: "../.env"});
+var MyToken = artifacts.require("./MyToken.sol");
+var MyTokenSales = artifacts.require("./MyTokenSale.sol");
+var KycContract = artifacts.require("./KycContract.sol");
 
-module.exports = async function(deployer) {
+require('dotenv').config({path: '../.env'});
+
+
+module.exports = async function(deployer) { 
   let addr = await web3.eth.getAccounts();
-  await deployer.deploy(MyToken, process.env.INITIAL_TOKENS);
-  let instance = await MyToken.deployed();
-  await deployer.deploy(MyTokenSale, 1, addr[0], MyToken.address);
- await instance.transfer(MyTokenSale.address, process.env.INITIAL_TOKENS);
-}
+await deployer.deploy(MyToken, process.env.INITIAL_TOKENS  );
+await deployer.deploy(KycContract);
+await deployer.deploy(MyTokenSales, 1, addr[0], MyToken.address, KycContract.address);
+let tokenInstance = await MyToken.deployed();
+await tokenInstance.transfer(MyTokenSales.address, process.env.INITIAL_TOKENS);
+};
